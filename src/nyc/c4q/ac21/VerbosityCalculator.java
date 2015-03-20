bd070c3dc04b41f52d69aaae7dfecb45e3506409
ahhhlvin <alvkuang@gmail.com>
2015-03-20 18:09:20 -0400

    verb calculator correct

--- src/nyc/c4q/ac21/VerbosityCalculator.java
+++ src/nyc/c4q/ac21/VerbosityCalculator.java
@@ -8,24 +8,63 @@ import java.util.Scanner;
  * Calculate the verbosity of a text
  */
 public class VerbosityCalculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        File TheProjectGutenberg = new File("/Users/alvin2/desktop/accesscode/VerbosityCalculator/resources/TheProjectGutenberg.rtf");
+        File ATaleofTwoCities = new File("/Users/alvin2/desktop/accesscode/VerbosityCalculator/resources/ATaleOfTwoCities.rtf");
+
+        System.out.println("The Project Gutenberg -- ");
+        verbCalculator(TheProjectGutenberg);
+
+
+        System.out.println("A Tale of Two Cities -- ");
+        verbCalculator(ATaleofTwoCities);
+
+
+    }
+
+
+    public static void verbCalculator(File file){
 
         // An abstract representation of a File
-        File file = new File("PATH to text file");
+        File text = new File("PATH to text file");
 
         try{
             Scanner sc = new Scanner(file);
-            /**
-             *  Your CODE goes here
-             *
-             *
-             *
-             *
-             */
+
+            String textFile;
+            int words = 0;
+            int sentences = 0;
+            double verbosity;
+
+
+            while (sc.hasNext()) {
+                textFile = sc.next();
+
+                words ++;
+
+                if ((textFile.equals("Dr.")) || (textFile.equals("Ms.")) || (textFile.equals("Mrs.")) || (textFile.equals("Mr."))) {
+                    continue;
+                } else if ( (textFile.contains(".")) || (textFile.contains("?")) || (textFile.contains("!")) ) {
+                    sentences++;
+                }
+
+
+                verbosity = words/(double)sentences;
+
+                
+                System.out.println(verbosity);
+
+
+            }
+
+
+
 
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
         }
+
     }
 }
